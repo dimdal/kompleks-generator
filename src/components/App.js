3f7357dfdd9fc7b28441b2f42a0f4c25a137810b
Marius Ørvik <dimdal@gmail.com>
2019-03-22 15:02:04 +0100

    added fb og tags

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ const App = () => {
           Kompleks-generator
           <div class="sub header">
             <div class="content">
-              Finn nye komplekser du kan bli deprimert over på 1-2-3!
+              Finn nye komplekser du kan blogge om på 1-2-3!
             </div>
           </div>
         </h1>
@@ -22,7 +22,9 @@ const App = () => {
       <div class="ui divider" />
       <div className="ui basic segment action_buttons">
         <a target="_blank" href="https://www.youtube.com/watch?v=qmmB_oWmRDU">
-          <button class="large ui teal basic button">Finn kirurg</button>
+          <button class="large ui teal basic button">
+            Bestill plastisk operasjon
+          </button>
         </a>
         <a target="_blank" href="https://sophieelise.blogg.no/">
           <button class="large ui pink basic button">Tips Sophie Elise</button>
